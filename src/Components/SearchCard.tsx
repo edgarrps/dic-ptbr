c4a040875fb295fa5dc6058f570cd1f78de6c02d
Edgar Santos <edgarrps@gmail.com>
2023-03-22 23:31:55 -0400

    initial layout

---
+++ src/Components/SearchCard.tsx
@@ -0,0 +1,11 @@
+import SearchInput from "./SearchInput"
+
+const SearchCard = () => {
+    return (
+        <div className='flex mx-auto mt-20 bg-blue-300 w-[700px] h-[300px] rounded-md'>
+            <SearchInput />
+        </div>
+    )
+}
+
+export default SearchCard
--- src/Pages/Dic.tsx
+++ src/Pages/Dic.tsx
@@ -1,7 +1,10 @@
+import SearchCard from "../Components/SearchCard";
+
 export default function Dic() {
     return (
-        <>
+        <div className='bg-cyan-100 h-screen w-screen'>
         <h1 className='text-center font-bold text-8xl'>Dicionário</h1>
-        </>
+        <SearchCard/>
+        </div>
     )
 }
