89b38b6ad5bc06819565f65581bdea4c982bb460
Edgar Santos <edgarrps@gmail.com>
2023-03-22 22:54:09 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Dic from './Pages/Dic'
+import './App.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Dic />
+  </React.StrictMode>
+)
---
+++ src/Pages/Dic.tsx
@@ -0,0 +1,7 @@
+export default function Dic() {
+    return (
+        <>
+        <h1 className='text-center font-bold text-8xl'>Dicionário</h1>
+        </>
+    )
+}
