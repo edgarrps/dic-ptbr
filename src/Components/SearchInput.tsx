d89c071704ac0a987974f7283312a28cdc7a1143
Edgar Santos <edgarrps@gmail.com>
2023-03-23 23:05:47 -0400

    bg added

--- src/Components/SearchInput.tsx
+++ src/Components/SearchInput.tsx
@@ -7,7 +7,7 @@ export default function SearchInput () {
                 <input className='bg-stone-100 rounded-l-full w-[360px] h-[30px]' type='text' />
                 <button className='bg-purple-100 rounded-r-full pl-2 pr-2 w-auto h-[30px] font-bold text-stone-500'>Busca</button>
             </div>
-            <ReactTextareaAutosize className='w-[400px] pl-2 pr-2 pt-1 pb-1 focus:outline-none resize-none' />
+            <ReactTextareaAutosize className='w-[400px] bg-cyan-400 pl-2 pr-2 pt-1 pb-1 focus:outline-none resize-none' />
         </div>
     )
 }
