ceae391892ce4ca1fa6a62c63fcf5c4cffbcb905
edgarrps <edgarrps@gmail.com>
2023-03-28 14:45:44 -0400

    linebreak regex

--- src/Components/SearchCard.tsx
+++ src/Components/SearchCard.tsx
@@ -16,7 +16,7 @@ export default function SearchCard() {
     const { word, handleChange, setWord }: Props = useContext(WordContext)
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => e.preventDefault()
     const [result, setResult] = useState([{}])
-
+    const quebra = ('.,').replace(/\r?\n/g, '<br/>') 
 
     async function handleSearch() {
         if (word === '') alert('Preencha uma palavra para buscar no dicionário')
@@ -31,7 +31,6 @@ export default function SearchCard() {
             }
         }
     }
-
 
     return (
         <div className='grid place-items-center p-6'>
@@ -49,16 +48,12 @@ export default function SearchCard() {
                         <button onClick={handleSearch} className='bg-purple-100 pl-2 pr-2 
                     h-[30px] font-bold text-stone-500'>Busca</button>
                     </p>
+
                     {Object.keys(result).length > 0 && (
                         <div className='w-[400px] bg-cyan-400
                      pl-2 pr-2 pt-10 pb-1 focus:outline-none resize-none'>
-                            <h1>{`Palavra: teste`}</h1>
-                            <h2>{`: ${result[0].meanings}`}</h2>
-                            --------
-                            <h2>{`${result[0].etymology.length > 0 ?
-                                `Etimologia: ${result[0].etymology}` : ''}`}</h2>
-
-
+                            <h2 className={`text-justify`}>{result[0].meanings ? quebra : ''}</h2>
+                            <h2>{`${result[0].etymology}`}</h2>
                         </div>
                     )}
                 </div>
